a8a8fb2fd942f0af1a9b80a1e9f3ab932b7caa94
Aditya Ardiya <1798412+ardiya@users.noreply.github.com>
2025-11-20 22:28:57 +0900

    fix: git ssh key

--- git/git_global_config.sh
+++ git/git_global_config.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Git global config
 
-# Signing keys are used to sign Git commits and tags. This process ensures the integrity and authenticity of your commits, verifying that they have not been tampered with and that they indeed come from you. Signing commits adds a layer of security, making it harder for malicious actors to impersonate you.
-git config --global user.signingkey ~/.ssh/id_rsa
+git config --global user.signingkey ~/.ssh/id_ed25519
+git config --global gpg.format ssh
