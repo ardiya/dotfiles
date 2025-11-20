5800ae17c10473a9803c2069fddd9003faf0704f
Aditya Ardiya <1798412+ardiya@users.noreply.github.com>
2025-11-20 22:30:52 +0900

    fix: git ssh key, should've used pub key lol

--- git/git_global_config.sh
+++ git/git_global_config.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Git global config
 
-git config --global user.signingkey ~/.ssh/id_ed25519
+git config --global user.signingkey ~/.ssh/id_ed25519.pub
 git config --global gpg.format ssh
